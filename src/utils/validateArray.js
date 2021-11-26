e3097332d14f356c92aaf4394678f43991fa1bd0
Josué Almonasi <josue.almonasi@hotmail.com>
2021-11-26 13:55:52 -0600

    Added function to validate the initial input

---
+++ src/utils/validateArray.js
@@ -0,0 +1,12 @@
+export const validateInput = (arr) => {
+  if (arr === null || arr === undefined) {
+    return true;
+  }
+  while (arr.length < 3) {
+    arr.push(null);
+    console.log(arr);
+  }
+  return arr[0] && validateInput(arr[1]) && validateInput(arr[2]);
+};
+
+//call like this  !!validateInput(testArray)
